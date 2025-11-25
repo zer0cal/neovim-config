74288f0209b65fe999cc5d91a7a797efed480895
Wojciech Falba <wojciech.falba@gmail.com>
2025-11-25 13:46:13 +0100

    add plugins

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,13 @@
+return {
+	{ -- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help ibl`
+		main = "ibl",
+		---@module "ibl"
+		---@type ibl.config
+		opts = {
+			indent = { char = "╎" },
+		},
+	},
+}
---
+++ lua/plugins/tiny-inline-diagnostic.lua
@@ -0,0 +1,21 @@
+return {
+    'rachartier/tiny-inline-diagnostic.nvim',
+    event = 'VeryLazy',
+    priority = 1000,
+    config = function()
+      require('tiny-inline-diagnostic').setup {
+        preset = 'classic',
+        options = {
+          show_source = {
+            enabled = true,
+            if_many = true,
+          },
+          multilines = {
+            enabled = true,
+            always_show = true,
+          },
+        },
+      }
+      vim.diagnostic.config { virtual_text = false } -- Disable Neovim's default virtual text diagnostics
+    end,
+}
